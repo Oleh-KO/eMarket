17cedcac15e49782aa8ba08c381e611ea4c67f98
Oleh Kosach <olezhkakosach@gmail.com>
2020-01-24 22:00:21 +0200

    Second commit

--- src/app/main/bucket/bucket.component.ts
+++ src/app/main/bucket/bucket.component.ts
@@ -9,20 +9,30 @@ export class BucketComponent implements OnInit {
 
   bucketArray = [];
   totalPrice: number = 0;
+  countItemsTwo: number = 0;
+
 
   constructor() { }
+
+  counter() {
+    this.bucketArray.forEach(element => {
+      this.countItemsTwo += element.count;
+    });
+  }
 
   deleteItem(i) {
     this.totalPrice -= this.bucketArray[i].price;
     this.bucketArray.splice(i, 1);
     localStorage.clear();
     localStorage.setItem("array", JSON.stringify(this.bucketArray));
+    this.countItemsTwo -= 1;
   }
 
   ngOnInit() {
     this.bucketArray = JSON.parse(localStorage.getItem("array"));
     this.bucketArray.forEach(element => {
       this.totalPrice += element.price;
+      this.countItemsTwo += element.count;
     });
   }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -12,46 +12,54 @@ export class MainComponent implements OnInit {
       "id": 1,
       "name": "Article 1",
       "label": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "price": 25
+      "price": 25,
+      "count": 1
     },
     {
       "id": 2,
       "name": "Article 2",
       "label": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "price": 35
+      "price": 35,
+      "count": 1
     },
     {
       "id": 3,
       "name": "Article 3",
       "label": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "price": 45
+      "price": 45,
+      "count": 1
     },
     {
       "id": 4,
       "name": "Article 4",
       "label": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "price": 55
+      "price": 55,
+      "count": 1
     },
     {
       "id": 5,
       "name": "Article 5",
       "label": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "price": 65
+      "price": 65,
+      "count": 1
     },
     {
       "id": 6,
       "name": "Article 6",
       "label": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "price": 75
+      "price": 75,
+      "count": 1
     }
   ];
 
   addedItems = [];
+  countItems: number = 0;
 
   constructor() { }
 
   addItem(i) {
     this.addedItems.push(this.itemArray[i]);
+    this.countItems += 1;
   }
 
   getItems() {
@@ -61,6 +69,9 @@ export class MainComponent implements OnInit {
   ngOnInit() {
     if (localStorage.getItem('array')) {
       this.addedItems = JSON.parse(localStorage.getItem("array"));
+      this.addedItems.forEach(element => {
+        this.countItems += element.count;
+      });
     }
   }
 
